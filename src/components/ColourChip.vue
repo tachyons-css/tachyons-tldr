b2e95d6e8b64c00574a1fc444c3a42d2ed368614
Varun Vachhar <varunvachhar@gmail.com>
2017-02-26 23:24:01 -0500

    📚

--- src/components/ColourChip.vue
+++ src/components/ColourChip.vue
@@ -10,12 +10,12 @@ export default {
 
 <template>
   <div>
-    <div class="h3 relative flex items-center justify-center br1 br--top overflow-hidden bt bl br b--black-10">
+    <div class="h3 relative flex items-center justify-center br1 br--top overflow-hidden bt br bl b--black-10">
       <div class="absolute top-0 right-0 bottom-0 left-0 bg-checkered" />
       <div class="absolute top-0 right-0 bottom-0 left-0"
         :style="{ backgroundColor: colour }" />
     </div>
-    <div class="bg-white flex-auto lh-copy pl2 pv1 ba b--black-10 br1 br--bottom">
+    <div class="bg-white flex-auto lh-copy pl2 pv1 bb br bl b--black-10 br1 br--bottom">
       <b class="db truncate dark-gray f6">{{ name }}</b>
       <code class="f6 truncate light-silver">{{ colour }}</code>
     </div>
--- src/views/PaletteView.vue
+++ src/views/PaletteView.vue
@@ -17,7 +17,7 @@ export default {
 
       <h2>Solid</h2>
       <div class="mb5 flex flex-wrap">
-        <colour-chip class="w-40 w-25-m w-25-l mb3 pr3 flex-auto"
+        <colour-chip class="w-50 w-33-m w-25-l mb3 pr3"
           v-for="(colour, name) in colours.solid"
           :colour="colour"
           :name="name" />
@@ -25,7 +25,7 @@ export default {
 
       <h2>Alpha</h2>
       <div class="flex flex-wrap">
-        <colour-chip class="w-40 w-25-m w-20-l mb3 pr2 flex-auto"
+        <colour-chip class="w-50 w-25-l mb3 pr3"
           v-for="(colour, name, index) in colours.alpha"
           :colour="colour"
           :name="name" />
